a0456e1d4ef6429406bd9719cee9018a8728efaa
Danrlei Martins <danrlei.martins@hotmail.com>
2022-11-30 12:37:39 +0000

    Check out & in, rent duration, deposit, set due

--- carRental.sol
+++ carRental.sol
@@ -49,4 +49,59 @@ contract CarRental {
             end
         );
     }
+
+    // Checkout a car
+    function rentCar(address walletAddress) public {
+        require(
+            renters[walletAddress].due == 0,
+            "You have a pending balance, please pay it."
+        );
+        require(
+            renters[walletAddress].canRent == true,
+            "You cannot rent at this time."
+        );
+        renters[walletAddress].active = true;
+        renters[walletAddress].start = block.timestamp;
+        renters[walletAddress].canRent = false;
+    }
+
+    // Check in a car
+    function returnCar(address walletAddress) public {
+        require(
+            renters[walletAddress].active == true,
+            "Please rent a car first."
+        );
+        renters[walletAddress].active = false;
+        renters[walletAddress].end = block.timestamp;
+
+        //Set rent amount due
+        setDue(walletAddress);
+    }
+
+    function getRentDuration(address walletAddress) public view returns (uint256){
+        require(
+            renters[walletAddress].active == false,
+            "Car is currently checked out (rented)."
+        );
+        //uint timespan = renterTimeSpan(renters[walletAddress].start, renters [walletAddress].end);
+        //uint timespanInMin = timespan / 60;
+        //return timespanInMin;
+        return 60;
+    }
+
+    // Check if can rent
+    function canRent(address walletAddress) public view returns (bool) {
+        return renters[walletAddress].canRent;
+    }
+
+    // Deposit to current balance
+    function deposit(address walletAddress) public payable {
+        renters[walletAddress].balance += msg.value;
+    }
+
+    // Set due amount (0.13 Ether/day, 0.0054 Ether/hour, 0.00009027777 Ether/min)
+    function setDue(address walletAddress) internal {
+        uint256 timespanMinutes = getRentDuration(walletAddress);
+        renters[walletAddress].due = timespanMinutes * 130000000000000000;
+    }
 }
